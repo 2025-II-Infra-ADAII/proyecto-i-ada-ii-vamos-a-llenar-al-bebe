5cb826c56b0042ea969191a57f7c3acab0cc69c7
daniel-gomez-cano <daniel.gomez.cano@correounivalle.edu.co>
2025-10-15 21:41:11 -0500

    Estructura basica inicial del proyecto, Actions basicas iniciales

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+//Funcion basica en java
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Es solo una funcion de prueba, no se asusten lol, obio va a fallar");
+    }
+}
